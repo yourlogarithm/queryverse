4c440d912c2302a5b5610e23f4a802a55c7615f0
Vlad-Sebastian Cretu <vlad.sebastian.cretu.dev@gmail.com>
2024-10-23 23:51:46 +0300

    setting up telemetry with quickwit

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,11 +1,12 @@
-use opentelemetry::{global, trace::TraceError, KeyValue};
+use opentelemetry::{global, KeyValue};
+use opentelemetry_appender_tracing::layer::OpenTelemetryTracingBridge;
+use opentelemetry_otlp::WithExportConfig;
 use opentelemetry_sdk::{
-    runtime,
+    logs::LoggerProvider,
     trace::{Config, TracerProvider},
     Resource,
 };
-use tracing_loki::url::Url;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Layer};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Layer, field::MakeExt};
 
 #[cfg(feature = "redis")]
 pub mod redis;
@@ -13,35 +14,64 @@ pub mod redis;
 #[cfg(feature = "database")]
 pub mod database;
 
-fn init_tracer_provider(pkg: &str) -> Result<TracerProvider, TraceError> {
-    let resource = Resource::new(vec![KeyValue::new("service.name", pkg.to_owned())]);
+fn get_resource(pkg: &str) -> Resource {
+    Resource::new(vec![KeyValue::new(
+        opentelemetry_semantic_conventions::resource::SERVICE_NAME,
+        pkg.to_owned(),
+    )])
+}
+
+fn init_tracer_provider(pkg: &str, endpoint: &str) -> TracerProvider {
     opentelemetry_otlp::new_pipeline()
         .tracing()
-        .with_exporter(opentelemetry_otlp::new_exporter().tonic())
-        .with_trace_config(Config::default().with_resource(resource))
-        .install_batch(runtime::Tokio)
+        .with_exporter(
+            opentelemetry_otlp::new_exporter()
+                .tonic()
+                .with_endpoint(endpoint),
+        )
+        .with_trace_config(
+            Config::default()
+                .with_resource(get_resource(pkg))
+        )
+        .install_batch(opentelemetry_sdk::runtime::Tokio)
+        .unwrap()
+}
+
+fn init_logs(pkg: &str, endpoint: &str) -> LoggerProvider {
+    opentelemetry_otlp::new_pipeline()
+        .logging()
+        .with_resource(get_resource(pkg))
+        .with_exporter(
+            opentelemetry_otlp::new_exporter()
+                .tonic()
+                .with_endpoint(endpoint),
+        )
+        .install_batch(opentelemetry_sdk::runtime::Tokio)
+        .unwrap()
 }
 
 pub async fn start(pkg: &str, f: std::pin::Pin<Box<dyn futures::Future<Output = ()>>>) {
-    let tracer = init_tracer_provider(pkg).unwrap();
-    global::set_tracer_provider(tracer);
-    let loki_url = std::env::var("LOKI_URL").unwrap();
-    let (layer, task) = tracing_loki::builder()
-        .label("service", pkg)
-        .unwrap()
-        .extra_field("pid", format!("{}", std::process::id()))
-        .unwrap()
-        .build_url(Url::parse(&loki_url).unwrap())
-        .unwrap();
-    tokio::spawn(task);
+    let otel_endpoint = std::env::var("OTEL_COLLECTOR").unwrap();
+
+    let tracer_provider = init_tracer_provider(pkg, &otel_endpoint);
+    global::set_tracer_provider(tracer_provider);
+
+    let logger_provider = init_logs(pkg, &otel_endpoint);
+    let layer = OpenTelemetryTracingBridge::new(&logger_provider);
+
     let filter = EnvFilter::from_default_env();
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::fmt::layer()
                 .without_time()
+                .map_fmt_fields(|f| f.debug_alt())
                 .with_filter(filter),
         )
         .with(layer)
         .init();
+
     f.await;
+
+    global::shutdown_tracer_provider();
+    logger_provider.shutdown().unwrap();
 }
